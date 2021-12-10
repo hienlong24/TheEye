74e7acb1d67318070e117062d4a2d7588b97446e
Hien Long <hienlong24@gmail.com>
2021-12-09 23:42:09 -0500

    Added djangrestframework. Fixed Event object creation. Changed data field name to info.

--- InteractionAnalytics/views.py
+++ InteractionAnalytics/views.py
@@ -4,14 +4,21 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from InteractionAnalytics.models import Event
 
+
 @api_view(['GET', 'POST'])
 def event(request):
     if request.method == 'POST':
-        event = request.data
-        print(event)
-        event = Event(data=request.data)
-        if event.is_valid():
-            event.save()
-            return Response(event.data, status=status.HTTP_201_CREATED)
-        return Response(event.errors, status=status.HTTP_400_BAD_REQUEST)
+        d= request.data
+        event = Event()
+        event.session_id = d['session_id']
+        event.category = d['category']
+        event.name = d['name']
+        event.info = d['data']
         
+        event.save()
+        return Response(event.data, status=status.HTTP_201_CREATED)
+
+    elif request.method == 'GET':
+        events = Event.objects.all()
+        print(events.__str__())
+        return Response(status=status.HTTP_201_CREATED)
